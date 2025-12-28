4a320306d16425e303358465c9721ac06aa2344f
Pablo Queiroz <pablo.hl2@gmail.com>
2025-12-28 17:40:57 +0000

    search-bar done

--- src/components/StudentsList.jsx
+++ src/components/StudentsList.jsx
@@ -3,34 +3,56 @@ import StudentCard from "./StudentCard";
 import { useEffect, useState } from "react";
 const itensPerPage = 8;
 
-function StudentsList({favorites = [], onToggleFavorite}) {
-const [students, setStudents] = useState([]);
-    
-    useEffect(() => {
-        axios.get("http://localhost:5005/students")
-        .then(({data}) => {
-            console.log("Students Data:", data);
-            setStudents(data || []);
-        })
-        .catch((error) => {
-            console.error("Error fetching students data:", error);
-        }); 
-        
-    }, []);
-    
-    const [currentPage, setCurrentPage] = useState(1);
-    const startIndex = (currentPage - 1) * itensPerPage;
-    const endIndex = startIndex + itensPerPage;
-    const totalPages = Math.ceil(students.length / itensPerPage);
-    const studentsToShow = students.slice(startIndex, endIndex);
+function StudentsList({ favorites = [], onToggleFavorite }) {
+  const [students, setStudents] = useState([]);
+
+  //search
+  const [search, setSearch] = useState("");
+  const filteredStudents = students.filter(
+    (student) =>
+      student.name.toLowerCase().includes(search.toLowerCase()) ||
+      student.house.toLowerCase().includes(search.toLowerCase())
+  );
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5005/students")
+      .then(({ data }) => {
+        console.log("Students Data:", data);
+        setStudents(data || []);
+      })
+      .catch((error) => {
+        console.error("Error fetching students data:", error);
+      });
+  }, []);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const startIndex = (currentPage - 1) * itensPerPage;
+  const endIndex = startIndex + itensPerPage;
+  const totalPages = Math.ceil(filteredStudents.length / itensPerPage);
+  const studentsToShow = filteredStudents.slice(startIndex, endIndex);
 
   return (
     <>
-    
+      <section className="search-bar-container">
+        <input
+          className="seach-bar-input"
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </section>
+      
+
       <div className="students-list">
         {studentsToShow.map((student) => (
-          <StudentCard key={student.id} student={student} isFavorite={favorites.some(fav => fav.id === student.id)}
-            onToggleFavorite={onToggleFavorite}/>
+          <StudentCard
+            key={student.id}
+            student={student}
+            isFavorite={favorites.some((fav) => fav.id === student.id)}
+            onToggleFavorite={onToggleFavorite}
+          />
         ))}
       </div>
 
